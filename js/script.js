2142b60840687d81609fa503b9209d11c96cffa5
iqnefo1310 <iqbalnafis13@gmail.com>
2024-06-30 17:53:27 +0700

    Delete js directory

--- js/script.js
+++
@@ -1,9 +0,0 @@
-const checkbox = document.getElementById('lock');
-        
-checkbox.addEventListener('change', function() {
-    if (checkbox.checked) {
-        document.body.classList.add('lock-scroll');
-    } else {
-        document.body.classList.remove('lock-scroll');
-    }
-});
